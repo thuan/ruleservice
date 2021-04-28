ef74f530591a4e3e8ac519550cca660e5ab256e8
Thuan Saraiva <thuan.saraiva@pathfind.com.br>
2021-04-28 17:17:17 -0300

    changes in classpathresources KieConfiguration and modifying drl files

--- src/main/java/com/itegra/auditcom/config/KieConfiguration.java
+++ src/main/java/com/itegra/auditcom/config/KieConfiguration.java
@@ -1,24 +1,43 @@
 package com.itegra.auditcom.config;
 
+import org.drools.core.io.impl.ClassPathResource;
+import org.drools.core.io.impl.FileSystemResource;
 import org.kie.api.KieServices;
 import org.kie.api.builder.KieBuilder;
 import org.kie.api.builder.KieFileSystem;
 import org.kie.api.builder.KieModule;
 import org.kie.api.runtime.KieContainer;
 import org.kie.internal.io.ResourceFactory;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class KieConfiguration {
 
-    public static final String drlFile = "rules/generalRules.drl";
+    private final Logger log = LoggerFactory.getLogger(KieConfiguration.class);
+
+    public static final String drlFile = "rules";
 
     @Bean
     public KieContainer kieContainer() {
         KieServices kieServices = KieServices.Factory.get();
         KieFileSystem kieFileSystem = kieServices.newKieFileSystem();
-        kieFileSystem.write(ResourceFactory.newClassPathResource(drlFile));
+        ClassPathResource classPathResource = new ClassPathResource(drlFile);
+        if(classPathResource.isDirectory()) {
+            classPathResource.listResources()
+                .forEach(file -> {
+                    FileSystemResource fResource = ((FileSystemResource)file);
+                    log.debug("Writting file: " + fResource.getFile().getName());
+                    kieFileSystem.write(fResource);
+                });
+        } else {
+            kieFileSystem.write(ResourceFactory.newClassPathResource(drlFile));
+        }
+
+        //kieFileSystem.write(ResourceFactory.newClassPathResource(drlFile));
+
         KieBuilder kieBuilder = kieServices.newKieBuilder(kieFileSystem);
         kieBuilder.buildAll();
         KieModule kieModule = kieBuilder.getKieModule();
