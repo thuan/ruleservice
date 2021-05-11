05e2800dc9dfe03c04adb0ab3926ff591865dd94
Rodrigo <hodrigocastelo@gmail.com>
2021-05-11 14:30:24 -0300

    atualizacoes nas correcoes de DRL

--- src/main/java/com/itegra/auditcom/service/TipoProdutoService.java
+++
@@ -1,9 +0,0 @@
-package com.itegra.auditcom.service;
-
-import com.itegra.auditcom.domain.AlimentoAcordanteEntrada;
-import com.itegra.auditcom.domain.ResultadoAlimentoAcordanteEntrada;
-
-public interface TipoProdutoService {
-
-    ResultadoAlimentoAcordanteEntrada resultado(AlimentoAcordanteEntrada ali, ResultadoAlimentoAcordanteEntrada res);
-}
