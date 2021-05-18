d11a2dfa3f42fd1e1e478de17f9a83269de70b38
Deyvison1 <tutacf@gmail.com>
2021-05-18 16:31:25 -0300

    Ajustado metodo get para post

--- src/main/java/com/itegra/auditcom/web/rest/DefineRegionResource.java
+++ src/main/java/com/itegra/auditcom/web/rest/DefineRegionResource.java
@@ -1,12 +1,9 @@
 package com.itegra.auditcom.web.rest;
 
-import java.io.IOException;
-import java.math.BigDecimal;
-
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.itegra.auditcom.domain.AlimentoAcordanteEntrada;
@@ -14,21 +11,14 @@ import com.itegra.auditcom.domain.ResultadoAlimentoAcordanteEntrada;
 import com.itegra.auditcom.service.KieService;
 
 @RestController
-@RequestMapping("define-region")
+@RequestMapping("/define-region")
 public class DefineRegionResource {
 
 	@Autowired
 	private KieService kieService;
 
-	@RequestMapping(value = "/resultado", method = RequestMethod.GET, produces = "application/json;charset=UTF-8")
-	public ResultadoAlimentoAcordanteEntrada getRegiao(@RequestParam(required = false) String ufEmitente,
-			@RequestParam(required = false) String ufDestinatario,
-			@RequestParam("percentualICMS") BigDecimal percentualICMS, @RequestParam(required = false) BigDecimal crt,
-			@RequestParam(required = false) BigDecimal valorItem, @RequestParam BigDecimal valorIPI,
-			@RequestParam(required = false) BigDecimal valorFrete,
-			@RequestParam(required = false) BigDecimal valorSeguro, @RequestParam BigDecimal valorOutros,
-			@RequestParam BigDecimal cst, @RequestParam BigDecimal categoria) throws IOException {
-		AlimentoAcordanteEntrada alimentoAcordanteEntrada = new AlimentoAcordanteEntrada(ufEmitente, ufDestinatario);
+	@RequestMapping(value = "/resultado", method = RequestMethod.POST, produces = "application/json;charset=UTF-8")
+	public ResultadoAlimentoAcordanteEntrada getRegiao(@RequestBody AlimentoAcordanteEntrada alimentoAcordanteEntrada) {
 		ResultadoAlimentoAcordanteEntrada resultadoAlimentoAcordanteEntrada = new ResultadoAlimentoAcordanteEntrada();
 		return kieService.resultado(alimentoAcordanteEntrada, resultadoAlimentoAcordanteEntrada);
 	}
